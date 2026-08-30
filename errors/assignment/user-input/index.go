fa57fed3919e2ec6cd0fa60d765acd0d238819c7
Rohan-Munot <rohanmunot24@gmail.com>
2026-08-30 16:56:29 +0530

    ERRORS

---
+++ errors/assignment/user-input/index.go
@@ -0,0 +1,22 @@
+// Complete the validateStatus function. It should return an error when the status update violates any of the rules:
+
+// If the status is empty, return an error that reads status cannot be empty.
+// If the status exceeds 140 characters, return an error that says status exceeds 140 characters.
+// Otherwise, return nil.
+
+package main
+
+import (
+	"errors"
+)
+
+func validateStatus(status string) error {
+	// ?
+	if status == "" {
+		return errors.New("status cannot be empty")
+	}
+	if len(status) > 140 {
+		return errors.New("status exceeds 140 characters")
+	}
+	return nil
+}
